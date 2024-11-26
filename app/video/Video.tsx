c28909e8a5096a35ac47693345746ba09b272549
samora <samora.sm.sam@gmail.com>
2024-11-26 07:45:13 +0200

    video fallback added

--- app/video/Video.tsx
+++ app/video/Video.tsx
@@ -40,7 +40,7 @@ export default function Video() {
       <div className="mb-4">
         <div className="relative mb-4">
           <video controls className="w-full rounded-lg">
-            <source src={video.video_url} type="video/mp4" />
+            <source src={video.video_url || ""} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
