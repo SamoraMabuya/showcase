b3b368b710496a8cb8433fd524f2ac7a8f42e17c
samora <samora.sm.sam@gmail.com>
2024-12-01 16:58:18 +0200

    routeItems created

---
+++ utils/config/index.ts
@@ -0,0 +1,22 @@
+import { HomeIcon, UserIcon, UploadIcon } from "@/lib/Icon";
+
+export const RouteItems = [
+  {
+    key: "homeId",
+    text: "Home",
+    href: "/",
+    icon: HomeIcon,
+  },
+  {
+    key: "profileId",
+    text: "Profile",
+    href: "/Profile",
+    icon: UserIcon,
+  },
+  {
+    key: "uploadId",
+    text: "Upload",
+    href: "/Upload",
+    icon: UploadIcon,
+  },
+];
