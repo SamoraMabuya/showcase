007fd0d8f858a2a6249b41448d91ffd57f783f31
samora <samora.sm.sam@gmail.com>
2024-12-01 16:56:17 +0200

    FilterSearch removed from Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,8 @@
 "use client";
-import NextLogo from "./NextLogo";
-import SupabaseLogo from "./SupabaseLogo";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { Button, buttonVariants } from "@/components/Button";
-import SideDrawer from "./Drawer";
+
 import { SearchBar } from "./SearchBar";
 import { AuthButton } from "./AuthButton";
-
-const FilterSearch = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="size-6"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
-    />
-  </svg>
-);
+import SideDrawer from "./Drawer/SideDrawer";
 
 export default function Header() {
   return (
