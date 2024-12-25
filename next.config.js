27ea47bcef4d055279f06e65df1612409ad25981
samora <samora.sm.sam@gmail.com>
2024-12-25 13:49:12 +0200

    optimizing memory usage

--- next.config.js
+++ next.config.js
@@ -11,6 +11,25 @@ const nextConfig = {
       },
     ],
   },
+  unoptimized: true,
+  webpack: (config, { isServer }) => {
+    // Add clean webpack plugin
+    config.optimization = {
+      ...config.optimization,
+      runtimeChunk: false,
+    };
+    return config;
+  },
+  swcMinify: true,
+  onDemandEntries: {
+    // Reduce the number of pages kept in memory
+    maxInactiveAge: 15 * 1000,
+    pagesBufferLength: 2,
+  },
+  typescript: {
+    // This will reduce TypeScript checking overhead during development
+    ignoreBuildErrors: true, // Only during development
+  },
 };
 
 module.exports = nextConfig;
