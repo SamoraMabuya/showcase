5fd25e2bdd449758481970308f1a630f0472b00b
samora <samora.sm.sam@gmail.com>
2024-12-26 02:06:32 +0200

    UseDropDownMedn added to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,19 +2,42 @@
 
 import { SearchBar } from "./SearchBar";
 import { AuthButton } from "./AuthButton";
-import SideDrawer from "./Drawer/SideDrawer";
+import { useSidebar } from "./ui/SideBar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/DropDownMenu";
+import { UserCircleIcon } from "@/lib/Icon";
+import { ThemeToggle } from "./Drawer/SideDrawer";
+
+const UserDropDown = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserCircleIcon />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <ThemeToggle />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 export default function Header() {
+  const { isMobile } = useSidebar();
   return (
     <div className="mx-4">
-      <div className="flex flex-row justify-between gap-16 mt-2 align-middle">
-        <div>{/* <SideDrawer /> */}</div>
-        <div>
-          <AuthButton />
-        </div>
-      </div>
-      <div className="my-10">
+      <div className=" flex flex-row-reverse md:flex-row ml-auto md:justify-between gap-4 mt-2 align-middle">
+        <UserDropDown />
+
         <SearchBar />
+        {!isMobile && <AuthButton />}
       </div>
     </div>
   );
